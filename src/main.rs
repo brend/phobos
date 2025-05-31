2aa92b977b5899460b131a8e3bf0ca187e01ad95
waldrumpus <waldrumpus@gmail.com>
2025-05-31 09:00:06 +0200

    move tests to mod test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use ast::TopLevelDecl;
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub phobos_grammar);
@@ -21,44 +20,50 @@ fn main() {
     codegen::generate_code(&mut handle, &program).expect("Failed to generate code");
 }
 
-#[allow(dead_code)]
-fn program_to_string(program: &Vec<TopLevelDecl>) -> String {
-    program
-        .iter()
-        .map(|decl| format!("{:?}", decl))
-        .collect::<Vec<String>>()
-        .join("\n")
-}
+#[cfg(test)]
+mod tests {
+    use super::ast::TopLevelDecl;
+    use super::phobos_grammar;
 
-#[test]
-fn test_parse_extern_decl() {
-    let code = "extern foo(n: Int): Int";
-    let program = phobos_grammar::ProgramParser::new()
-        .parse(code)
-        .expect("Failed to parse program");
-    let stringified = program_to_string(&program);
+    #[allow(dead_code)]
+    fn program_to_string(program: &Vec<TopLevelDecl>) -> String {
+        program
+            .iter()
+            .map(|decl| format!("{:?}", decl))
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
 
-    assert_eq!(stringified, code);
-}
+    #[test]
+    fn test_parse_extern_decl() {
+        let code = "extern foo(n: Int): Int";
+        let program = phobos_grammar::ProgramParser::new()
+            .parse(code)
+            .expect("Failed to parse program");
+        let stringified = program_to_string(&program);
 
-#[test]
-fn test_parse_function_decl() {
-    let code = "fn foo(n: Int): Int { return (n + 1); }";
-    let program = phobos_grammar::ProgramParser::new()
-        .parse(code)
-        .expect("Failed to parse program");
-    let stringified = program_to_string(&program);
+        assert_eq!(stringified, code);
+    }
 
-    assert_eq!(stringified, code);
-}
+    #[test]
+    fn test_parse_function_decl() {
+        let code = "fn foo(n: Int): Int { return (n + 1); }";
+        let program = phobos_grammar::ProgramParser::new()
+            .parse(code)
+            .expect("Failed to parse program");
+        let stringified = program_to_string(&program);
 
-#[test]
-fn test_parse_type_decl() {
-    let code = "type Foo { name: String, age: Int }";
-    let program = phobos_grammar::ProgramParser::new()
-        .parse(code)
-        .expect("Failed to parse program");
-    let stringified = program_to_string(&program);
+        assert_eq!(stringified, code);
+    }
 
-    assert_eq!(stringified, code);
+    #[test]
+    fn test_parse_type_decl() {
+        let code = "type Foo { name: String, age: Int }";
+        let program = phobos_grammar::ProgramParser::new()
+            .parse(code)
+            .expect("Failed to parse program");
+        let stringified = program_to_string(&program);
+
+        assert_eq!(stringified, code);
+    }
 }
